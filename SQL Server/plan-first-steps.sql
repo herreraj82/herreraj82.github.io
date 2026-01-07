5796d7b07ba52d1ab16f54340800e1e3661c65c6
herreraj82 <herrera.j82@gmail.com>
2026-01-07 18:50:45 -0500

    Added first steps for dql server instance

---
+++ SQL Server/plan-first-steps.sql
@@ -0,0 +1,71 @@
+/*
+
+need to create:
+    retail orders
+        order header
+        order details
+    retail prods
+        retail products
+        brand
+        manufacturer
+        category
+        broad category
+    locations
+        shipping
+            from
+            to
+        billing
+        dba
+        mfg
+    retail shipments
+        shipment header
+        shipment details
+    shipment journey
+        carrier
+        container of some kind?
+        stops
+        source
+        destination
+    geo
+        cities/munic
+        state/prov/subfederal subdivision
+        intra-country region
+        country
+        inter-country region
+        continent
+        global region
+    hr 
+        employees
+        departments
+        offices
+        cst rls managers
+        employee status change events
+        schedules
+    it
+        equipment
+        equipment assignments
+    suppliers
+        purchase orders
+        purchase shipments
+    marketing
+        campaigns
+        tests
+        partners
+        quotes
+        leads
+    forecasts
+        manual
+        statistical
+        budgets
+        goals
+    mfg
+        raw materials
+        intermidiate prods
+        finish prods
+        demand 
+        lots
+
+    helper    
+        integers
+        dates
+*/
